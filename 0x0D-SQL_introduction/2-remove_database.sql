43b02d9127d8f598541f88dff34e4fbd00be612d
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-08-17 11:54:19 -0700

    Intro to SQL

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- deletes the database hbtn_0c_0
+-- If the database hbtn_0c_0 doesn’t exist, script should not fail
+-- you are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
