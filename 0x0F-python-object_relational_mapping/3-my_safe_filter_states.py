23a269708815d44b36c1502fcb7b0dc06f3e42fe
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-16 06:58:50 -0700

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Once again, write a script that takes in arguments and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument. But this time,
+write one that is safe from MySQL injections!
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db_host = "localhost"
+    db_user = sys.argv[1]  # "your_username"
+    db_password = sys.argv[2]  # "your_password"
+    db_name = sys.argv[3]  # "your_database_name"
+    port = 3306
+    state_name = sys.argv[4]  # "your_database_name"
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    params = (state_name,)
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=3306
+    )
+    cursor = db.cursor()
+
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
