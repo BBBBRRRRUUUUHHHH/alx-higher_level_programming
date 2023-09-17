c9204ed761c06eeb729628234210797f3ed797bd
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-17 11:10:53 -0700

    task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 """
 Write a python file that contains the class definition
-of a State and an instance Base = declarative_base():
+of a State and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class City(Base):
-    """Represents a city for a MySQL database.
+class State(Base):
+    """ class class """
+    __tablename__ = 'states'
 
-    Attributes:
-        id (str): The city's id.
-        name (sqlalchemy.Integer): The city's name.
-        state_id (sqlalchemy.String): The city's state id.
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
