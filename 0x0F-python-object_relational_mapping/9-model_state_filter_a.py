eb2059ee11c5990e458af5567c95e5d8ecdaf214
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-17 11:37:19 -0700

    task9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that lists all State objects
-that contain the letter a from the database hbtn_0e_6_usa
+Write a script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
