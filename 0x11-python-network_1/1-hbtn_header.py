a5b270bd6adec9d836574ef7339ace9e8a33fdd9
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-30 06:18:54 -0700

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,6 @@ variable found in the header of the response.
 import urllib.request
 import sys
 url = sys.argv[1]
-
-
 with urllib.request.urlopen(url) as response:
     x_request_id = response.getheader('X-Request-Id')
     print(x_request_id)
