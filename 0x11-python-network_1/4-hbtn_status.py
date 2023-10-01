b583fda546f480f45b9a585bc94567d9ccb008e2
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-10-01 06:30:13 -0700

    What's my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,9 +3,9 @@
 Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
-
-url = "https://alx-intranet.hbtn.io/status"
-r = requests.get(url)
-print("Body response:")
-print("\t- type:", type(r))
-print("\t- content:", r)
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(r))
+    print("\t- content:", r)
