6e266ad302efe929834b20f315760b26cf55dd61
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-10-01 10:35:43 -0700

    Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+import requests
+import sys
+url = requests.get(sys.argv[1])
+if __name__ == '__main__':
+    if url.status_code >= 400:
+        print("Error code:", url.status_code)
+    else:
+        print(url.text)
