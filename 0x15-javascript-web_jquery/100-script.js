c4b728cb75c3bf75355291632a49ea3cd337a090
Brian <125502769+BBBBRRRRUUUUHHHH@users.noreply.github.com>
2023-11-02 01:38:20 +0300

    Update 100-script.js

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,7 +1,3 @@
-// Wait for the DOM to be ready
 document.addEventListener('DOMContentLoaded', function () {
-  // Select the <header> element
-  const header = document.querySelector('header');
-  // Update its text color to red
-  header.style.color = '#FF0000';
+  document.querySelector('header').style.color = '#FF0000';
 });
