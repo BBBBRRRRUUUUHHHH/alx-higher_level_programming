f7a4ca6169fcce81142e98c58fd24192f4639516
Brian <125502769+BBBBRRRRUUUUHHHH@users.noreply.github.com>
2023-11-02 01:24:47 +0300

    Update 5-script.js

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,4 @@
-$(document).ready(function () {
-  $('#add_item').click(function () {
-    $('.my_list').append('<li>Item</li>');
-  });
+// Jquery API that adds an <li> to <ul> on click
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
 });
