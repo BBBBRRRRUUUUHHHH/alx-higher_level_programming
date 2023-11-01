4e5d508d311ccf819e516044e090eb531fbd04e3
Brian <125502769+BBBBRRRRUUUUHHHH@users.noreply.github.com>
2023-11-02 01:22:44 +0300

    Update 3-script.js

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,4 @@
-$(function () {
-  $('#red_header').click(function () {
-    $('header').addClass('red');
-  });
+// Jquery API that adds a class on the header tag
+$('DIV#red_header').click(function () {
+  $('header').addClass('red');
 });
